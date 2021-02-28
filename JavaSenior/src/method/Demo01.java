8332f82aa9f45af694df7b2aeef71c6ef1b0971b
王根生 <2913173310@qq.com>
2021-02-28 14:18:16 +0800

    JavaSenior

---
+++ JavaSenior/src/method/Demo01.java
@@ -0,0 +1,11 @@
+package method;
+
+/**
+ * @author wgsstart
+ * @creat 2020-12-13 19:50
+ */
+public class Demo01 {
+    public static void main (String[] args){
+        Student student = new Student();
+    }
+}
---
+++ JavaSenior/src/method/Student.java
@@ -0,0 +1,13 @@
+package method;
+
+/**
+ * @author wgsstart
+ * @creat 2020-12-14 16:46
+ */
+public class Student {
+    String name;
+    int age;
+    public void study(){
+        System.out.println(this.name+"在学习");
+    }
+}
